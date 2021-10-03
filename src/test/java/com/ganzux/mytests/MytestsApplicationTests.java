ca492ff72c5b619d0c94f9fcedd91643cfdab6b2
alvaro <itoito@gmail.com>
2021-10-03 22:37:13 +0200

    First empty version

---
+++ src/test/java/com/ganzux/mytests/MytestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganzux.mytests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
